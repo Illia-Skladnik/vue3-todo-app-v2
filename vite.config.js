3d8c9ab06c2a823e5643257bb5de98f9024fd73c
Illia Skladnik <scladnik@gmail.com>
2023-05-07 13:50:17 +0300

    dev

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue3-todo-app-v2/',
+  
   plugins: [vue()],
   resolve: {
     alias: {
